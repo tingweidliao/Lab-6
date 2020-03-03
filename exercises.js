6cab7204e799da18c0afca71580171d27519d2d7
tingweidliao <59951624+levid24@users.noreply.github.com>
2020-03-02 23:56:17 -1000

    Create exercises.js
    update

---
+++ exercises.js
@@ -0,0 +1,15 @@
+<script>
+
+age = 26;
+fav_num = 24;
+day_of_birth = 24;
+month_of_birth = 11;
+
+document.getElementById("output_span").innerHTML=(age + fav_num / day_of_birth * month_of_birth);
+
+age + fav_num / day_of_birth * month_of_birth
+
+
+
+
+</script>
